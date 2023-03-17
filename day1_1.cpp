c731cb62dc23d52ca4af13af82bda3bddb08ee7c
DivyBhanderi05 <123169804+DivyBhanderi05@users.noreply.github.com>
2023-03-17 17:08:18 +0530

    Add files via upload

---
+++ day1_1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int n;
+	
+	cout<<"Enter number of Elements: ";
+	cin>>n;
+	
+	int arr[n],i;
+	
+   for(i=0 ; i<n ; i++){
+   	 cout<<"enter your elemets: ";
+     cin>>arr[i];
+     }
+     
+     cout<<"\n even number are : ";
+     for(i=0; i<n; i++){
+     	
+      if(arr[i]%2==0)
+         cout<<arr[i]<<" ";
+   }
+   cout<<endl;
+
+return 0;
+}
---
+++ day1_3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int i;
+	int arr[100];
+	
+	cout<<"Leap year between 2020 and 3030 are : "<<endl;
+	
+	for(i=2020 ; i<=3030 ; i++){
+		
+		if(i%4==0){
+			cout<<i;
+			
+		}
+		cout<<"   ";
+		
+	}
+	
+	
+	
+	return 0;
+}
